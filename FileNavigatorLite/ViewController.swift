2964c1c531e54ed47a919046b17d2fbd6b4a5c9c
Miroshnichenko Marina <marinamirosh99@gmail.com>
2020-05-23 19:11:02 +0300

    Initial Commit

---
+++ FileNavigatorLite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FileNavigatorLite
+//
+//  Created by Мирошниченко Марина on 23.05.2020.
+//  Copyright © 2020 Мирошниченко Марина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
